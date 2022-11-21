fe8cb553e33edc9f01521989951c9ab07a6651c5
aniktonu <aniktonu@gmail.com>
2022-11-21 17:36:10 +0600

    folder change

---
+++ MyApp/component.js
@@ -0,0 +1,21 @@
+export let a = 45;
+export let str = "Intellier";
+export let obj = {
+    name: "Intellier",
+    age: 45
+}
+
+let arr = [1, 2, 3, 4, 5, 6];
+
+export let hello = () => {
+    console.log("Hello World!");
+};
+
+export class Person {
+    constructor(name, age) {
+        this.name = "anik";
+        this.age = 20;
+    }
+}
+
+export default arr;
