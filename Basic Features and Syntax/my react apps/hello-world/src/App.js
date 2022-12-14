319648e0e046877956c3434e4bab3d1a916d0bc1
aniktonu <aniktonu@gmail.com>
2022-12-14 18:02:32 +0600

    practice

--- Basic Features and Syntax/my react apps/hello-world/src/App.js
+++ Basic Features and Syntax/my react apps/hello-world/src/App.js
@@ -1,25 +1,67 @@
-import React, {Component} from 'react';
-import Book from './Components/Book';
-import './App.css';
-// import Person from './Components/Person';
+import React, { Component } from 'react'
+import Book from './Components/Book'
+import './App.css'
 
-
-// //class component
 class App extends Component {
-render () {
 
-// let test = new Component();
-// console.log(test);
+// constructor () {
+//     super();
+//     this.state = {
+//       books: []
+//     }
+//   }
 
-   return (
-    <div className='App'>
-      <h1>Book List</h1>
-    <Book bookName="1986" author="john doe"/>
-    <Book bookName="1985" author="john cee"/>
-    <Book bookName="1984" author="john koe"/>
-    </div>
-   )
+state = {
+    books: [
+      {bookname: 'The Alchemist', author: 'Paulo Coelho'},
+      {bookname: 'The Monk Who Sold His Ferrari', author: 'Robin Sharma'},
+      {bookname: 'The Power of Your Subconscious Mind', author: 'Joseph Murphy'}
+    ],
+    otherState: 'some other value'
+
 }
+
+changeState = (newBookName) => {
+  this.setState({
+    books: [
+      {bookname: newBookName, author: 'gtrg g rtgh'},
+      {bookname: ' gt tgt h thghr', author: ' htrh'},
+      {bookname: 'T rhrth rh', author: 'hhhh rthrthr '}
+    ]
+  })
 }
+
+chnageWithInputState = (event) => {
+  this.setState({
+    books: [
+      {bookname: event.target.value, author: 'gtrg g rtgh'},
+      {bookname: ' gt tgt h thghr', author: ' htrh'},
+      {bookname: 'T rhrth rh', author: 'hhhh rthrthr '}
+    ]
+  })
+}
+
+  render() {
+
+    return (
+      <div className='App'>
+        <h1>Book List</h1>
+        <button onClick={this.changeState.bind(this,"anik hasan")}>Change State</button>
+        <input  type="text" onChange={this.chnageWithInputState }/>
+        <Book bookname={this.state.books[0].bookname} and 
+        author={this.state.books[0].author}
+        inputName={this.chnageWithInputState}/>
+        <Book bookname={this.state.books[1].bookname} and 
+        author={this.state.books[1].author}/>
+        <Book bookname={this.state.books[2].bookname} and 
+        author={this.state.books[2].author} 
+        change={this.changeState.bind(this,"The Alchemist")}
+        />
+      </div>
+    )
+  }
+}
+
+
 
 export default App;
