6ba9ee9c8b0c711611274f0560b8ffa25d2afcd9
aniktonu <aniktonu@gmail.com>
2022-12-14 18:02:14 +0600

    PRACTICE

--- Basic Features and Syntax/my react apps/hello-world/src/Components/Book.js
+++ Basic Features and Syntax/my react apps/hello-world/src/Components/Book.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react';
+import './Book.css';
 
-
-const Book= (props) =>{
- return (
-<div>
-    <h3>Book Name : {props.bookName}</h3>
-    <h4>Author : {props.author}</h4>
-</div>
- )
+const Book = (props) =>{
+  return (
+    <div className='Book'>
+        
+        <h3 onClick={props.change}> Book : {props.bookname}</h3>
+        <h4>Writer :  {props.author}</h4>
+        <input type="text" onChange={props.inputName} value={props.bookname}/>
+    </div>
+  )
 }
 
 
